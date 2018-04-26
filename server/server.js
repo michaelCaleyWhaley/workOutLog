4f0b737bf315775e59fadfb0823c42ef750707fb
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2018-04-26 17:21:23 +0100

    Adding GET by Id route

--- server/server.js
+++ server/server.js
@@ -9,11 +9,11 @@ var app = express();
 
 app.use(bodyParser.json());
 
+// POST request creating a new todo entry using the Todo model
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
-
     todo.save().then((doc) => {
         res.send(doc);
     }, (error) => {
@@ -21,11 +21,22 @@ app.post('/todos', (req, res) => {
     });
 });
 
+// GET request returning all todos
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos});
+        res.send({ todos });
     }, (error) => {
         res.status(400).send(error);
+    });
+});
+
+// GET request returning todos from specific IDs
+app.get('/todos/:id', (req, res) => {
+    if (!req.params.id) return res.send('Must include Id in query.');
+    Todo.findById(req.params.id).then((collection) => {
+        res.send(collection);
+    }).catch((error) => {
+        res.status(404).send(error.message);
     });
 });
 
