5136f2ec65e170eeb8cd8058607572b90a628f54
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2018-05-10 12:11:03 +0100

    mongo connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 // Because promises used to be a third party addition to JS you must tell mongoose to use the built in promise
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.mongoDB_URI || 'mongodb://localhost:27017/TodoApp');
+// mongoose.connect(process.env.mongoDB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.mongoDB_URI);
 
 module.exports = {
     mongoose
