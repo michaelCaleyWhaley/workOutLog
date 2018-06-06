a589a4cd7ea00449c302d352c931b8ea179f4701
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2018-06-06 12:02:38 +0100

    Add POST /Users route and token method

--- server/server.js
+++ server/server.js
@@ -95,6 +95,21 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+// POST /users
+app.post('/users', (req, res) => {
+    var userProperties = _.pick(req.body, ['email', 'password']);
+    var user = new User(userProperties);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
+
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
