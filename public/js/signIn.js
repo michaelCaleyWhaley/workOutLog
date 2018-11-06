20225584fc361d91506b39610d2bf42a62dbebfa
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2018-11-06 13:30:05 +0000

    serialise form data

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -1,18 +1,17 @@
+var signInForm = document.getElementsByClassName("sign-in__form")[0];
+var signInSubmit = document.getElementsByClassName("sign-in__submit")[0];
 
-var signInForm = document.getElementsByClassName('sign-in__form')[0];
-var signInSubmit = document.getElementsByClassName('sign-in__submit')[0];
-var inputFields = signInForm.getElementsByTagName('input');
-var labelFields = signInForm.getElementsByTagName('label');
+function serialiseForm(form) {
+  var input = form.getElementsByTagName("input");
+  var formData = {};
+  for (var i = 0; i < input.length; i++) {
+    formData[input[i].name] = input[i].value;
+  }
+  return formData = JSON.stringify(formData);
+}
 
-signInSubmit.addEventListener('click', function(e){
+signInSubmit.addEventListener("click", function(e) {
   e.preventDefault();
 
-  var data = {};
-
-  for(var i = 0; i < inputFields.length; i++){
-    data.labelFields[i].innerText = inputFields[i].value;
-  }
-
-  console.log(data);
-
+  serialiseForm(signInForm);
 });
